24e88739a29f4f9c68bc3f76d6c3e14f72521d9c
Electra51 <safayetnurelectra@gmail.com>
2022-11-09 20:10:08 +0600

    service post api added

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
             res.send(services);
         });
 
+        app.post('/services',  async (req, res) => {
+            const service = req.body;
+            console.log(service);
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        });
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -54,6 +61,19 @@ async function run() {
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         });
+
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const status =req.body.status
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status:status
+                }
+            }
+            const result = await reviewCollection.updateOne(query,updatedDoc);
+            res.send(result);
+        })
 
         app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
